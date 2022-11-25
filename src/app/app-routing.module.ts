f4f769bdfe7ed316472cb9f5782786cc6f7a055d
joshuadada <jdbabatunde98@gmail.com>
2022-11-25 13:15:26 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'hotel', loadChildren: () => import('./hotel/hotel.module').then(m => m.HotelModule) }, 
+  { path: 'cake', loadChildren: () => import('./cake/cake.module').then(m => m.CakeModule) },
+  { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
