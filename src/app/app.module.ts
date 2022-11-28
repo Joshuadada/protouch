550b61b0e6489dab2f4f4eacb1ee947ee06dba46
joshuadada <jdbabatunde98@gmail.com>
2022-11-28 10:22:43 +0100

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
 
@@ -12,4 +13,109 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  imagesStore = [
+    {
+      id: '1',
+      src: '../../../../assets/home/header-1.png'
+
+    },
+    {
+      id: '2',
+      src: '../../../../assets/home/header-1.png'
+
+    },
+    {
+      id: '3',
+      src: '../../../../assets/home/header-1.png'
+
+    }
+  ]
+  imagesStore2 = [
+    {
+      id: '1',
+      src: '../../../../assets/home/header-2.png'
+
+    },
+    {
+      id: '2',
+      src: '../../../../assets/home/header-2.png'
+
+    },
+    {
+      id: '3',
+      src: '../../../../assets/home/header-2.png'
+
+    }
+  ]
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    autoplay: true,
+    autoplaySpeed: 700,
+    navText: ['<div class="bg-[#FFFEFE] w-32 cursor-pointer text-center text-[#E44200] py-2 px-3 text-sm">Previous</div>', '<div class="text-[#FFFEFE] w-32 cursor-pointer text-center bg-[#E44200] py-2 px-3 text-sm">Next</div> '],
+    responsive: {
+      0: {
+        items: 1
+      },
+    },
+    nav: true
+  }
+  customOptions2: OwlOptions = {
+    loop: true,
+    
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    autoplay: true,
+    autoplaySpeed: 700,
+    navText: ['<div class="bg-[#FFFEFE] w-32 cursor-pointer text-center text-[#E44200] py-2 px-3 text-sm">Previous</div>', '<div class="text-[#FFFEFE] w-32 cursor-pointer text-center bg-[#E44200] py-2 px-3 text-sm">Next</div> '],
+    responsive: {
+      0: {
+        items: 1
+      },
+    },
+    nav: false,
+  }
+  customOptions3: OwlOptions = {
+    loop: true,
+    
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    autoplay: true,
+    autoplaySpeed: 700,
+    navText: ['<div class="bg-[#FFFEFE] w-32 cursor-pointer text-center text-[#E44200] py-2 px-3 text-sm">Previous</div>', '<div class="text-[#FFFEFE] w-32 cursor-pointer text-center bg-[#E44200] py-2 px-3 text-sm">Next</div> '],
+    responsive: {
+      0: {
+        items: 1
+      },
+    },
+    nav: false
+  }
+  customOptions4: OwlOptions = {
+    loop: true,
+    
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    autoplay: true,
+    autoplaySpeed: 700,
+    navText: ['<div class="absolute top-1/2 -translate-y-1/2 left-0 w-4 md:w-6 cursor-pointer"><img src="../../assets/home/arrow-left.png" class="w-full" alt="" /></div>', '<div class="absolute top-1/2 -translate-y-1/2 right-0 w-4 md:w-6 cursor-pointer"><img src="../../assets/home/arrow-right.png" class="w-full" alt="" /></div>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+    },
+    nav: true
+  }
 }
